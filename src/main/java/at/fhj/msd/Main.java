37b7ce493c721d0014644a42e60fdd209f4c20dc
andistraum <andreas.armanious@edu.fh-joanneum.at>
2023-06-01 10:42:25 +0200

    added log4j2 to project

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,7 +1,14 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private final static Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("This is an info log entry.");
+        logger.error("This is an error log entry.");
+
         Calculator calculator = new Calculator();
 
         System.out.println(calculator.add(1,1));
